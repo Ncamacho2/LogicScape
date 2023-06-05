76efbbf8d4b5061a15b5a0cc62bff5e2f87b8715
Nelson Camacho <ccan999@hotmail.com>
2023-06-05 18:31:50 -0500

    junit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ module AppLogicScape {
 	requires java.persistence;
 	requires com.fasterxml.jackson.annotation;
 	requires java.sql;
-	requires org.mockito;
 	
 	opens logicscape to javafx.graphics, javafx.fxml;
 }
--- src/test/java/logicscape/controladores/InicioControllerTest.java
+++ src/test/java/logicscape/controladores/InicioControllerTest.java
@@ -1,16 +1,9 @@
 package logicscape.controladores;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import logicscape.vistas.InicioView;
 
 
