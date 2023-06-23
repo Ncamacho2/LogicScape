172b30f8b60a513e454551944a9c37c73b6adba6
TomasSuarez <83978153+tomasdevelopment@users.noreply.github.com>
2023-06-22 19:49:52 -0500

    Rtas y botones preguntas

--- src/main/java/logicscape/controladores/RetoController.java
+++ src/main/java/logicscape/controladores/RetoController.java
@@ -2,23 +2,48 @@ package logicscape.controladores;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import java.util.List;
+import java.util.ArrayList;
 import logicscape.vistas.RetoView;
 
 public class RetoController {
 	
 	private RetoView retoView;
+	private RespuestasController respuestasController;
+	private String correctAnswer = "4"; 
+    private List<String> answers = new ArrayList<>();
 
-    public RetoController(RetoView retoView) {
-        this.retoView = retoView;
-        this.retoView.setRetoController(this);
+	public RetoController(RetoView retoView) {
+            this.retoView = retoView;
+            this.retoView.setRetoController(this);
+            // Add possible answers
+            answers.add("2");
+            answers.add("3");
+            answers.add("4");
+            answers.add("5");
     }
 
-    private void mostrarMensaje(String mensaje) {
-        Alert alert = new Alert(AlertType.INFORMATION);
-        alert.setTitle("Ventana emergente");
-        alert.setHeaderText("InformaciÃ³n");
-        alert.setContentText(mensaje);
-        alert.showAndWait();
+    // Method to set RespuestasController
+    public void setRespuestasController(RespuestasController respuestasController) {
+        this.respuestasController = respuestasController;
     }
 
+	public List<String> getAnswers() {
+		return answers;
+	}
+	public String getCorrectAnswer() {
+	    return correctAnswer;
+	}
+
+	public boolean checkAnswer(String userAnswer) {
+		return correctAnswer.equals(userAnswer);
+	}
+
+    public void mostrarMensaje(String mensaje) {
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Ventana emergente");
+            alert.setHeaderText("Información");
+            alert.setContentText(mensaje);
+            alert.showAndWait();
+    }
 }
---
+++ src/main/java/logicscape/vistas/RespuestasView.java
@@ -0,0 +1,51 @@
+package logicscape.vistas;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import logicscape.controladores.RespuestasController;
+
+public class RespuestasView extends Application {
+
+    private RespuestasController respuestasController;
+    private Label answerLabel;
+    private Scene scene;
+    private StackPane layout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        answerLabel = new Label();
+        layout = new StackPane();
+        layout.getChildren().add(answerLabel);
+        scene = new Scene(layout, 300, 200);
+        
+        scene.getStylesheets().add("file:src/main/resources/css/application2.css");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        // Initiate controller after all your view components have been set up
+        if (respuestasController != null) {
+            respuestasController.setRespuesta(respuestasController.getRespuesta()); // use the respuesta already set in the controller
+            respuestasController.iniciar();
+        }}
+       
+
+    public Scene getScene() {
+        return this.scene;
+    }
+
+    public void setRespuestasController(RespuestasController respuestasController) {
+        this.respuestasController = respuestasController;
+    }
+
+    public void setAnswer(String answer, boolean isCorrect) {
+        answerLabel.setText(answer);
+        if (isCorrect) {
+            layout.getStyleClass().add("green-background");
+        } else {
+            layout.getStyleClass().add("red-background");
+        }
+    }
+}
