af0a326ad91822e593e34fa315335cec486e1022
TomasSuarez <83978153+tomasdevelopment@users.noreply.github.com>
2023-06-24 21:40:06 -0500

    Comentarios/Javadoc

--- src/main/java/logicscape/controladores/RespuestasController.java
+++ src/main/java/logicscape/controladores/RespuestasController.java
@@ -16,93 +16,96 @@ import javafx.geometry.Pos;
 
 public class RespuestasController {
 
-	private RespuestasView respuestasView;
-	private Stage stage;
-	private String respuesta;
-	private Usuario usuario;
+    // Declaración de variables privadas
+    private RespuestasView respuestasView;
+    private Stage stage;
+    private String respuesta;
+    private Usuario usuario;
 
-	public RespuestasController(RespuestasView respuestasView, Stage stage, Usuario usuario) {
-		this.respuestasView = respuestasView;
-		this.usuario = usuario;
-		this.stage = stage;
+    // Constructor de la clase RespuestasController
+    public RespuestasController(RespuestasView respuestasView, Stage stage, Usuario usuario) {
+        this.respuestasView = respuestasView;
+        this.usuario = usuario;
+        this.stage = stage;
 
-		this.respuestasView.setRespuestasController(this);
-	}
+        // Establecer el controlador de respuestas en respuestasView
+        this.respuestasView.setRespuestasController(this);
+    }
 
-	public void setRespuesta(String respuesta) {
-		this.respuesta = respuesta;
-	}
+    /**
+     * @param respuesta la respuesta a establecer
+     */
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
 
-	public String getRespuesta() {
-		return this.respuesta;
-	}
+    // Getter para la respuesta
+    public String getRespuesta() {
+        return this.respuesta;
+    }
 
-	private void mostrarMensaje(String mensaje, boolean isCorrect) {
-	    Alert alert = new Alert(AlertType.INFORMATION);
-	    alert.setTitle("Ventana emergente");
-	    alert.setHeaderText(null); // Remove header text
+    // Método para mostrar un mensaje de alerta con una imagen y un mensaje basado en si la respuesta es correcta
+    private void mostrarMensaje(String mensaje, boolean isCorrect) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Ventana emergente");
+        alert.setHeaderText(null); 
 
-	    DialogPane dialogPane = alert.getDialogPane();
-	    dialogPane.setPrefSize(600, 400); // Set preferred size for dialogPane
+        DialogPane dialogPane = alert.getDialogPane();
+        dialogPane.setPrefSize(600, 400); 
 
-	    ImageView imageViewMarciano;
+        ImageView imageViewMarciano;
 
-	    // Escoger la imagen apropiada basada en si la rta es correcta
-	    if (isCorrect) {
-	        // Use the happy marciano image when the answer is correct
-	        imageViewMarciano = new ImageView(new Image(getClass().getResource("/img/marciano.png").toExternalForm()));
-	    } else {
-	        // Usar el marciano triste en la imagen que la rta es incorrecta. 
-	        imageViewMarciano = new ImageView(new Image(getClass().getResource("/img/sadmarciano.png").toExternalForm()));
-	    }
+        if (isCorrect) {
+            imageViewMarciano = new ImageView(new Image(getClass().getResource("/img/marciano.png").toExternalForm()));
+        } else {
+            imageViewMarciano = new ImageView(new Image(getClass().getResource("/img/sadmarciano.png").toExternalForm()));
+        }
 
-	    imageViewMarciano.setFitWidth(100); // Set width
-	    imageViewMarciano.setFitHeight(100); // Set height
+        imageViewMarciano.setFitWidth(100); 
+        imageViewMarciano.setFitHeight(100); 
 
-	    // Create an ImageView for the new image and set its size
-	    ImageView imageViewFondo2 = new ImageView(new Image(getClass().getResource("/img/fondo_2.png").toExternalForm()));
-	    imageViewFondo2.setFitWidth(100); // Definir Ancho 
-	    imageViewFondo2.setFitHeight(100); // Definir alto
+        ImageView imageViewFondo2 = new ImageView(new Image(getClass().getResource("/img/fondo_2.png").toExternalForm()));
+        imageViewFondo2.setFitWidth(100);
+        imageViewFondo2.setFitHeight(100);
 
-	    // Crear un nodo de texto para el mensaje en una fuente mas grande
-	    Text textNode = new Text(mensaje);
-	    textNode.setStyle("-fx-font-size: 30px; -fx-fill: white;"); // Set the font size and color
+        Text textNode = new Text(mensaje);
+        textNode.setStyle("-fx-font-size: 30px; -fx-fill: white;"); 
 
-	    // Crea un vbox que contenga la imagen y el mensaje 
-	    VBox vbox = new VBox(imageViewMarciano, imageViewFondo2, textNode);
-	    vbox.setAlignment(Pos.CENTER); // Center align the contents
+        VBox vbox = new VBox(imageViewMarciano, imageViewFondo2, textNode);
+        vbox.setAlignment(Pos.CENTER);
 
-	    //Poner el vbox y el contenido del dialogo de panel
-	    dialogPane.setContent(vbox);
+        dialogPane.setContent(vbox);
 
-	    // Aplicar el estilo CSS
-	    if (isCorrect) {
-	        dialogPane.setStyle("-fx-background-color: green;");
-	    } else {
-	        dialogPane.setStyle("-fx-background-color: red;");
-	    }
+        if (isCorrect) {
+            dialogPane.setStyle("-fx-background-color: green;");
+        } else {
+            dialogPane.setStyle("-fx-background-color: red;");
+        }
 
-	    alert.showAndWait();
-	}
+        alert.showAndWait();
+    }
 
+    // Método para iniciar el controlador, muestra un mensaje y carga la vista de nivel
+    public void iniciar() {
+        if (respuesta != null) {
+            boolean isCorrect = respuesta.equals("Correcto!");
+            System.out.println("Respuesta: " + respuesta);
+            System.out.println("isCorrect: " + isCorrect);
+            this.mostrarMensaje(respuesta, isCorrect);
+            NivelView nivelView = new NivelView();
+            NivelController nivelController = new NivelController(nivelView, usuario, 0);
+            nivelView.start(stage);
+        }
+    }
 
-	public void iniciar() {
-		if (respuesta != null) {
-			boolean isCorrect = respuesta.equals("Correcto!"); // "Correcto!" means the answer is correct
-			System.out.println("Respuesta: " + respuesta); // Debug print statement
-			System.out.println("isCorrect: " + isCorrect); // Debug print statement
-			this.mostrarMensaje(respuesta, isCorrect);
-			NivelView nivelView = new NivelView();
-			NivelController nivelController = new NivelController(nivelView, usuario, 0);
-			nivelView.start(stage);
-		}
-	}
-
-	public void displayResult(String result) {
-		boolean isCorrect = result.equals("Correcto!");
-		System.out.println("Result: " + result); // Debug print 
-		System.out.println("isCorrect: " + isCorrect); // Debug print 
-		this.mostrarMensaje(result, isCorrect);
-	}
-
+    /**
+     * Método para mostrar el resultado
+     * @param result el resultado a mostrar
+     */
+    public void displayResult(String result) {
+        boolean isCorrect = result.equals("Correcto!");
+        System.out.println("Result: " + result);
+        System.out.println("isCorrect: " + isCorrect);
+        this.mostrarMensaje(result, isCorrect);
+    }
 }
--- src/main/java/logicscape/vistas/RespuestasView.java
+++ src/main/java/logicscape/vistas/RespuestasView.java
@@ -9,6 +9,7 @@ import logicscape.controladores.RespuestasController;
 
 public class RespuestasView extends Application {
 
+    // Definir variables privadas que se utilizarán en la clase
     private RespuestasController respuestasController;
     private Label answerLabel;
     private Scene scene;
@@ -16,32 +17,53 @@ public class RespuestasView extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        // Crear una etiqueta para contener la respuesta
         answerLabel = new Label();
+
+        // Crear un layout y agregar la etiqueta de respuesta a él
         layout = new StackPane();
         layout.getChildren().add(answerLabel);
+
+        // Crear una escena con el layout definido y las dimensiones
         scene = new Scene(layout, 300, 200);
         
+        // Agregar una hoja de estilos CSS a la escena
         scene.getStylesheets().add("file:src/main/resources/css/application2.css");
+
+        // Establecer la escena creada en el primaryStage y mostrarla
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        // Iniciar controlador luego de crear componentes 
+        // Inicializar el controlador después de crear los componentes 
         if (respuestasController != null) {
-            respuestasController.setRespuesta(respuestasController.getRespuesta()); // usar respuesta creado en el set del controller
+            // Usar la respuesta creada en el setter del controlador
+            respuestasController.setRespuesta(respuestasController.getRespuesta()); 
             respuestasController.iniciar();
-        }}
-       
+        }
+    }
 
+    // Getter para la escena
     public Scene getScene() {
         return this.scene;
     }
 
+    /**
+     * @param respuestasController el controlador de respuestas a establecer
+     */
     public void setRespuestasController(RespuestasController respuestasController) {
         this.respuestasController = respuestasController;
     }
 
+    /**
+     * Método para establecer la respuesta y su corrección
+     * @param answer la respuesta a establecer
+     * @param isCorrect si la respuesta es correcta o no
+     */
     public void setAnswer(String answer, boolean isCorrect) {
+        // Establecer el texto de answerLabel a la respuesta proporcionada
         answerLabel.setText(answer);
+
+        // Dependiendo de la corrección de la respuesta, cambie el color de fondo del layout
         if (isCorrect) {
             layout.getStyleClass().add("green-background");
         } else {
